713c2be5492a2bf0487342d59066aa352cec0947
oalexallan <allexallansilveira@gmail.com>
2024-02-20 00:15:54 -0300

    Update, livros next.js, finalizado 96%.

--- livros-next/pages/_app.tsx
+++ livros-next/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+    <Head>
+      <meta name='viewport' content='width=devide-width, initial-scale=1' />
+    </Head>
+    <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
